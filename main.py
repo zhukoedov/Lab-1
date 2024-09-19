abd3a8c860a8f2842d1a7320f2e342d0671db511
zhukoedov <466837@niuitmo.ru>
2024-09-19 15:18:56 +0300

    changed main.py

--- main.py
+++ main.py
@@ -1,1 +1,15 @@
-print('\x1b[38;5;157;1m Hello world!')
+def drawline(offset = 0, length = 1):
+    line = ' ' * length
+    print(f'{" " * abs(offset)}\x1b[48;5;42m{line}\x1b[0m')
+
+def romb(height = 30):
+    offset = height // 2
+    for line in range(0, height):
+        drawline(offset, height - (2 * abs(line - height // 2)) - 1)
+        offset -= 1
+        
+
+
+if __name__ == "__main__":
+    romb(20)
+    romb(70)
